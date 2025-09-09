006847d1b810500f5c96326589e7b82721c6c88a
JeongHyun Kim <j456ay@gmail.com>
2025-09-09 14:04:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,28 @@
+# rulebook_constrainer/setup.py
 from setuptools import setup
-import os
-from glob import glob
 
 package_name = 'rulebook_constrainer'
 
 setup(
     name=package_name,
-    version='1.0.0',
-    packages=[package_name, f'{package_name}/utils'],
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), 
-         glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'config'), 
-         glob(os.path.join('config', '*.yaml'))),
+        ('share/' + package_name + '/config', ['config/params.yaml']),
+        ('share/' + package_name + '/launch', ['launch/rulebook_constrainer.launch.py']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'requests'],  # ⬅️ 추가
     zip_safe=True,
-    maintainer='Robot SW Engineer',
-    maintainer_email='developer@example.com',
-    description='ROS2 package for generating rule-based constraint masks from YAML rulebook for Nav2 integration',
+    maintainer='you',
+    maintainer_email='you@example.com',
+    description='Rulebook -> costmap masks (with LLM semantic compiler)',
     license='MIT',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rulebook_constrainer_node = rulebook_constrainer.rulebook_constrainer_node:main',
+            'rulebook_constrainer = rulebook_constrainer.rulebook_constrainer_node:main',
         ],
     },
 )
